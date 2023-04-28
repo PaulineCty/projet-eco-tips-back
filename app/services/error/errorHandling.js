cc0cab695ed1e005bb3c3bb046e7d79b9562935a
PaulineCty <paulinecauty@gmail.com>
2023-04-28 17:29:33 +0200

    better error management

--- app/services/error/errorHandling.js
+++ app/services/error/errorHandling.js
@@ -10,27 +10,8 @@ const errorModule = {
      * @param {function} __ Express' function executing the succeeding middleware
      */
     async manage(err, _, res, __) {
-        // if(!err.message) {
-        //     switch (err.code) {
-        //         case 400:
-        //             res.status(400).json({ message : "Bad request" });
-        //             break;
-        //         case 404:
-        //             res.status(404).json({ message : "Not found" });
-        //             break;
-        //         case 401:
-        //             res.status(401).json({ message : "Unauthorized" });
-        //             break;
-        //         case 500:
-        //             res.status(500).json({ message : "Internal Server Error" });
-        //             break;
-        //         default:
-        //             res.status(err.code).json({ message : "Internal server error" });
-        //             break;
-        //     }
-        // } else {
-            return res.status(err.code).json({ message : err.message });
-        // }
+            debug(err);
+            return res.status(err.code).json({ message : err.message});
     },
 
     /**
